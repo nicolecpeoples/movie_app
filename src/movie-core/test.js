3ffa2f920bb8b4ca83950a2c0ba32f1d369e645a
Nicole Peoples <npeople@NPEOPLE05740.local>
2017-06-12 15:22:08 -0700

    another test

--- src/movie-core/test.js
+++ src/movie-core/test.js
@@ -1,8 +1,4 @@
 angular.module('movieCore', ['ngResource'])
     .factory('PopularMovies', function ($resource){
-       return $resource('popular/:movieId', {movieId: '@id'}, {
-           update: {
-               method: 'PUT'
-           }
-       })
+     
     });
